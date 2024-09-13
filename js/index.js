d020ca359870763936eb2f16a7ddfde1bd80eb79
WegleC <weglechen@gmail.com>
2024-09-13 19:17:58 +0800

    call fn from the other fn in fn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
-function myfn(){
-        a = 2;
-        b = 3;
-        c = a*b;
-        console.log(c)
-        document.getElementById('num').innerHTML=c;
+function cal(){
+        price = document.getElementById('price').value;
+        num = document.getElementById("num").value;
+        totals = cal2(price,num);
+        // alert(price);
+        // alert(num);
+        //alert(totals);
+
+        document.getElementById("total").value = totals;
+
     }
 
+function cal2(a,b){
+    c = a * b;
+    return c;
+}

9f0f2c54f7c3178414e0d3ef5909b2de655a033d
WegleC <weglechen@gmail.com>
2024-09-14 18:43:43 +0800

    use for loop to find_all 'h3'

--- py/crawler_2.py
+++ py/crawler_2.py
@@ -10,26 +10,12 @@ def main():
     
     soup = BeautifulSoup(resp.text,'html.parser');
     
-    print("======1======")
+    main_title = soup.find_all('h3');
+    # print(main_title);
     
-    print(soup.find('h3').text);
-    
-    print("======2======")
-
-    print(soup.h3.text)
-    
-    print("======3======")
-    
-    print(soup.h3.a.text)
-
-    print("======4======")
-
-    print(soup.h3.string)
-    
-    print("======5======")
-
-    print(soup.h3.a.string)
-
+    for title in main_title:
+        print(title.text);
+        print("====================================================")
 
 
 

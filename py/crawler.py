dd4dab8dc565157bc590ada31f11af85f0fbb3af
WegleC <weglechen@gmail.com>
2024-09-14 17:34:16 +0800

    def fn & scraping

--- py/crawler.py
+++ py/crawler.py
@@ -6,13 +6,19 @@ from bs4 import BeautifulSoup
 def main():
     
     print("Hello world!");
+    url = 'https://www.yannick.com.tw/'
+    text = get_tag_text(url,'h3');
+    print(text);
+
+#%%
+
 
 
 def get_tag_text(url,tag):
     try:
         resp = requests.get(url)
         if resp.status_code == 200:
-        soup = BeautifulSoup(resp.text,'html.parser')
+            soup = BeautifulSoup(resp.text,'html.parser');
         return soup.find(tag).text
     except Exception as e:
         print('Error: %s' % e);

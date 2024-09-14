afc112d37e8eb3981ed2a9d9b3c707a636d5b78d
WegleC <weglechen@gmail.com>
2024-09-14 18:30:44 +0800

    find 'h3' text

--- py/crawler_2.py
+++ py/crawler_2.py
@@ -1,5 +1,55 @@
-# from crawler import myfn1
+import requests
+from bs4 import BeautifulSoup
 
-# print("Hello!!")
+#%%
 
-# myfn1()
+def main():
+    
+    resp = requests.get('https://www.dotblogs.com.tw/YiruAtStudio');
+    print(resp.status_code);
+    
+    soup = BeautifulSoup(resp.text,'html.parser');
+    
+    print("======1======")
+    
+    print(soup.find('h3').text);
+    
+    print("======2======")
+
+    print(soup.h3.text)
+    
+    print("======3======")
+    
+    print(soup.h3.a.text)
+
+    print("======4======")
+
+    print(soup.h3.string)
+    
+    print("======5======")
+
+    print(soup.h3.a.string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+if __name__ == "__main__":
+    main();
+

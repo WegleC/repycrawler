c162d9ced031ce3122527abe89523da3e436f4b3
WegleC <weglechen@gmail.com>
2024-09-16 18:39:57 +0800

    scrap text in div tags

--- py/crawler_2.py
+++ py/crawler_2.py
@@ -6,17 +6,18 @@ from bs4 import BeautifulSoup
 def main():
     
     resp = requests.get('https://blog.castman.net/py-scraping-analysis-book/ch2/blog/blog.html');
-    print(resp.status_code);
+    # print(resp.status_code);
     
     soup = BeautifulSoup(resp.text,'html.parser');
     
-    main_title = soup.find('a',{"data-foo":"mac-foo"});
-    print(main_title);
-    print("====================================");
-    print(main_title.text);
-
-
-
+    divs = soup.find_all('div','content');
+    # print(divs)
+    
+    for div in divs:
+        print("=============")
+        print(div.h6.text.strip())
+        print(div.a.text.strip())
+        print(div.p.text.strip())
 
 
 

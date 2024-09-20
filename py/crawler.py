ee14e8f85961627c2b1b014085641b5d50fb385f
WegleC <weglechen@gmail.com>
2024-09-20 20:51:24 +0800

    dict review

--- py/crawler.py
+++ py/crawler.py
@@ -1,11 +1,29 @@
-import requests
+
 
 #%%
 
-r = requests.get('https://raw.githubusercontent.com/wsmwason/taiwan-bank-code/master/data/taiwanBankCodeATM.json')
-data = r.json()
+def main():
+    my_dict = {
+        'a' : '46',
+        'b' : '50',
+        'c' : '98',
+        'd' : '83',
+        'e' : '76',
+    }
+    
+    for key_1 in my_dict.keys():
+        print(key_1)
+        
+    for value_1 in my_dict.values():
+        print(value_1)
+    
+    for key,value in my_dict.items():
+        print(key)
+        print(value)
+        print("===============")
 
-# print(data)
-for bank_info in data:
-    print("BankCode:",bank_info['code'])
-    print("Bank:",bank_info['name'])
+
+#%%
+
+if __name__ == "__main__":
+    main();

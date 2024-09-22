bbbaeaf0b233e670e5b808e1e539859384c43a86
WegleC <weglechen@gmail.com>
2024-09-22 17:32:54 +0800

    chinese in json key deal theory

--- py/crawler.py
+++ py/crawler.py
@@ -1,24 +1,27 @@
 import requests
-
+import json
 #%%
 
 def main():
-    r = requests.get('https://vbs.sports.taipei/opendata/sports_tms2.json')
-    data = r.json()
+    r = requests.get('https://stats.moe.gov.tw/files/school/113/school08_new.json')
     
-    # print(data)
-    for info in data:
-        print("場地所需分區:",info['Area'])
-        print("場地名稱:",info['Name'])
-        print("英文場地名稱:",info['NameEng'])
-        print("運動型態:",info['SportType'])
-        print("地址:",info['Address'])
-        print("英文地址：",info['AddressEng'])
-        print("開放時間:",info['startTime'])
-        print("關閉時間:",info['endTime'])
-        print("設施科聯絡電話:",info['LocalCallService'])
-        print("=======================================")
-    
+    text = r.content.decode('utf-8-sig')
+
+    data = json.loads(text)
+    print(data)
+
+    for school in data:
+        print("學年度:",school['學年度'])
+        print("學校級別:",school['學校級別'])
+        print("學校代碼:",school['學校代碼'])
+        print("學校名稱",school['學校名稱'])
+        print("郵遞區號",school['郵遞區號'])
+        print("地址",school['地址'])
+
+        print("===============================")
+
+
+
     
     
     

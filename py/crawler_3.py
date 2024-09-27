847afaf431aeafdff004408439081873c62c8cd2
WegleC <weglechen@gmail.com>
2024-09-27 22:34:58 +0800

    first marriage age api

--- py/crawler_3.py
+++ py/crawler_3.py
@@ -1,14 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
 
 #%%
 
 def main():
-    r = requests.get('https://member.lccnet.com.tw/Courses/5020240813213034')
-    # print(r.status_code)
+    r = requests.get('https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-002072-044')
+    print(r.status_code)
     
-    soup = BeautifulSoup(r.text,'html.parser')
-    print(soup)
     
 
 

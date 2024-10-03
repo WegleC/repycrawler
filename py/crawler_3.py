f4522361d5890cac7df5412dd9ded2f473fbce67
WegleC <weglechen@gmail.com>
2024-10-03 18:38:57 +0800

    Update crawler_3.py
    .

--- py/crawler_3.py
+++ py/crawler_3.py
@@ -6,7 +6,12 @@ def main():
     r = requests.get('https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-002072-044')
     print(r.status_code)
     
+    data = r.json()
+    # print(data)
     
+    for key, value in data.items():
+        print(key)
+
 
 
 #%%

d1c4f73d378b0be588ad384a4387da5fd5a3dd75
WegleC <weglechen@gmail.com>
2024-10-11 22:52:37 +0800

    ca5 init

--- py/crawler_5.py
+++ py/crawler_5.py
@@ -1,8 +1,24 @@
 import requests
-
+import json
 #%%
+#國民中學名錄
 def main():
-    r = requests.get('')
+    r = requests.get('https://stats.moe.gov.tw/files/school/112/j1_new.json');
+    
+    text = r.content.decode('utf-8-sig');
+    
+    data = json.loads(text);
+    
+    # print(data);
+    
+    for school in data:
+        print("學校名稱:",school['學校名稱'])
+        print("地址:",school['地址'])
+        print("電話:",school['電話'])
+        print("網址:",school['網址'])
+        print("=========================================")
+
+    
 
 
 
